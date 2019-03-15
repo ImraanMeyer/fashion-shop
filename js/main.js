658f2166ecc3e780f1682c1a0419fd39bc9b058c
ImraanMeyer <imraan.meyer97@gmail.com>
2019-03-15 21:12:04 +0200

    images updated

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function loadImg(){
+    var allimages= document.getElementsByTagName('img');
+    for (var i=0; i<allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+}, false)
+
+setTimeout(function loadImg() {
+
+}, 500)
